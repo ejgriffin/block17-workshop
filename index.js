5294a9148fe01fcc3a921ea4f2712bcba9571c2a
Eric Griffin <ejgriffin13@gmail.com>
2023-10-04 19:45:49 -0400

    created a function to print an array of the drink names

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 const coffeeMenu = require("./coffee_data.js");
-console.log(coffeeMenu);
+
+// Print array of all drinks on the menu
+// Print array of just the drink names.
+// Print array of the map function
+
+const printNames = (item) => {
+  return item.name;
+};
+const coffeeNames = coffeeMenu.map(printNames);
+
+console.log(coffeeNames);
