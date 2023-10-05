d6df72dc948ab9ac47e362145f292d5279b1b425
Eric Griffin <ejgriffin13@gmail.com>
2023-10-05 17:18:14 -0400

    changed some things and did console.table

--- index.js
+++ index.js
@@ -2,57 +2,57 @@ const coffeeMenu = require("./coffee_data.js");
 
 // Print array of all drinks on the menu
 // Print array of just the drink names.
-// Print array of the map function
+// use the map function
 
-const printNames = (item) => {
+const drinkNames = (item) => {
   return item.name;
 };
-const coffeeNames = coffeeMenu.map(printNames);
+const coffeeNames = coffeeMenu.map(drinkNames);
 
-// console.log(coffeeNames);
+console.log(coffeeNames);
+console.table(coffeeNames);
 
 //  Print array of drinks that cost 5 and under
-// -- filter function
+// use filter function
 
-const fiveAndUnder = coffeeMenu.filter((item) => {
+const fiveAndBelow = coffeeMenu.filter((item) => {
   return item.price <= 5;
 });
 
-// console.log(fiveAndUnder.map(printNames));
+console.log(fiveAndBelow.map(drinkNames));
+console.table(fiveAndBelow.map(drinkNames));
 
 // Print array of drinks that are priced at an even number
 // filter function
 
-const evenPrice = coffeeMenu.filter((item) => {
+const evenValue = coffeeMenu.filter((item) => {
   return item.price % 2 === 0;
 });
-// console.log(evenPrice.map(printNames));
+console.log(evenValue.map(drinkNames));
+console.table(evenValue.map(drinkNames));
 
 //Print the total if you were to order one of every drink.
 // reduce function
 
-const orderTotal = coffeeMenu.reduce((acc, currentValue) => {
+const fullMenu = coffeeMenu.reduce((acc, currentValue) => {
   return acc + currentValue.price;
 }, 0);
 
-// console.log(orderTotal);
+console.log(fullMenu);
+console.table(fullMenu);
 
 // Print an array with all the drinks that are seasonal.
 // use filter function
 
-const seasonalDrinks = coffeeMenu.filter((item) => {
+const seasonalBeverages = coffeeMenu.filter((item) => {
   return item.seasonal;
 });
-// console.log(seasonalDrinks.map(printNames));
-
-// coffeeMenu.map(item =>{
-//     console.log(`${item.name} with imported beans!`)
-// })
+console.log(seasonalBeverages.map(drinkNames));
+console.table(seasonalBeverages.map(drinkNames));
 
 //Print all the seasonal drinks with the words "with imported beans" after the item name. For example: "affogato with imported beans".
-// use map function and const for seasonalDrinks
+// use map function and const for seasonalBeverages
 
-seasonalDrinks.map((item) => {
+seasonalBeverages.map((item) => {
   console.log(`${item.name} with imported beans`);
 });
-console.log(seasonalDrinks.map(printNames));
