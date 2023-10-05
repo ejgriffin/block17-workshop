92e96c3e62edc5e71f1bea3ea580dbcb66c075fb
Eric Griffin <ejgriffin13@gmail.com>
2023-10-04 20:34:55 -0400

    created methods for printing new arrays from coffee data

--- index.js
+++ index.js
@@ -9,4 +9,43 @@ const printNames = (item) => {
 };
 const coffeeNames = coffeeMenu.map(printNames);
 
-console.log(coffeeNames);
+// console.log(coffeeNames);
+
+//  Print array of drinks that cost 5 and under
+// -- filter function
+
+const fiveAndUnder = coffeeMenu.filter((item) => {
+  return item.price <= 5;
+});
+
+// console.log(fiveAndUnder.map(printNames));
+
+// Print array of drinks that are priced at an even number
+// filter function
+
+const evenPrice = coffeeMenu.filter((item) => {
+  return item.price % 2 === 0;
+});
+// console.log(evenPrice.map(printNames));
+
+//Print the total if you were to order one of every drink.
+// reduce function
+
+const orderTotal = coffeeMenu.reduce((acc, currentValue) => {
+  return acc + currentValue.price;
+}, 0);
+
+// console.log(orderTotal);
+
+// Print an array with all the drinks that are seasonal.
+
+const seasonalDrinks = coffeeMenu.filter((item) => {
+  return item.seasonal;
+});
+// console.log(seasonalDrinks.map(printNames));
+
+// coffeeMenu.map(item =>{
+//     console.log(`${item.name} with imported beans!`)
+// })
+
+seasonalDrinks;
